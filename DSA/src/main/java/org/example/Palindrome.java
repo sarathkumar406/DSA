1f198c13f2dc3d9b3cddfe297095b3dde1e26bcf
Sarath <sarathkumar.406@gmail.com>
2025-01-18 21:54:07 +0530

    Palindrome Program logic

--- DSA/src/main/java/org/example/Palindrome.java
+++ DSA/src/main/java/org/example/Palindrome.java
@@ -2,12 +2,15 @@ package org.example;
 
 import static org.example.ReverseString.reverseString;
 
+
+// 2. Check if a String is a Palindrome, Ignoring Case
 public class Palindrome {
 
     public static void main(String[] args) {
 
         String str = "liril";
-
+        boolean res = checkPalindrome(str);
+        System.out.println(res);
         String str1 = reverseString(str);
 
         if(str.equalsIgnoreCase(str1)){
@@ -16,5 +19,20 @@ public class Palindrome {
             System.out.println("Its not a palindrome");
         }
 
+    }
+
+    private static boolean checkPalindrome(String str) {
+
+        int Start = 0;
+        int end = str.length()-1;
+
+        while(Start < end ){
+            if(str.charAt(Start) != str.charAt(end)){
+                return false; // Not a Palindrome
+            }
+            Start++;
+            end--;
+        }
+        return true; // Its a Palindrome
     }
 }
